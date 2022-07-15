6f1acd3b265e59bf460b2705e5efa4eeacda4b32
Aymen BAROUDI <baroudi.aymen@gmail.com>
2022-07-15 10:42:06 +0200

    first commit

---
+++ src/main/java/com/aybaroud/DevopsGitJenkinsDockerDockerhubKuberneteesApplication.java
@@ -0,0 +1,13 @@
+package com.aybaroud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsGitJenkinsDockerDockerhubKuberneteesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsGitJenkinsDockerDockerhubKuberneteesApplication.class, args);
+	}
+
+}
