e966fc51561783a98703b8f3166e4f5b7ba93059
Aymen BAROUDI <baroudi.aymen@gmail.com>
2022-07-21 17:01:04 +0200

    add new features to improve the project

--- src/main/java/com/aybaroud/DevopsGitJenkinsDockerDockerhubKuberneteesApplication.java
+++ src/main/java/com/aybaroud/DevopsGitJenkinsDockerDockerhubKuberneteesApplication.java
@@ -2,9 +2,17 @@ package com.aybaroud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DevopsGitJenkinsDockerDockerhubKuberneteesApplication {
+
+	@GetMapping
+	public String getProjectName(){
+		return "devops-git-jenkins-docker-dockerhub-kubernetees";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsGitJenkinsDockerDockerhubKuberneteesApplication.class, args);
